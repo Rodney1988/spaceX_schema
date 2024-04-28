f0d103ef9aa527d8ff90940473254f1aa1616157
Rodrigo Wahl <wahl@artech-is.com>
2024-04-28 12:11:07 +0200

    feat: add navbar logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { HomePage } from './components/HomePage';
 import { NavBar } from './components/NavBar';
+import { Missions } from './components/Missions';
 
 export const client = new ApolloClient({
   uri: 'https://spacex-production.up.railway.app/',
@@ -11,10 +13,15 @@ export const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <NavBar />
-      <HomePage />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/missions" element={<Missions />} />
+        </Routes>
+      </ApolloProvider>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,32 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
 export const NavBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathName = location.pathname;
+
   return (
     <nav className="bg-black shadow">
-      <div>
-        <h1 className="text-white text-3xl py-2 pl-2 font-bold">SPACEX API</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-white text-3xl py-2 pl-4 font-bold">SPACEX API</h1>
+        <ul className="flex flex-row space-x-4 pr-4">
+          <li
+            onClick={() => navigate('/')}
+            className={`text-white hover:text-gray-300 cursor-pointer ${
+              pathName === '/' ? 'text-gray-300' : ''
+            }`}
+          >
+            Launches
+          </li>
+          <li
+            onClick={() => navigate('/missions')}
+            className={`text-white hover:text-gray-300 cursor-pointer ${
+              pathName === '/missions' ? 'text-gray-300' : ''
+            }`}
+          >
+            Missions
+          </li>
+        </ul>
       </div>
     </nav>
   );
